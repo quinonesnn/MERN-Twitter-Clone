74488cc8649c2b443133fd824a0c83069a42070e
quinonesnn <quinonesnn@gmail.com>
2024-05-02 13:55:55 -0400

    project setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.routes.js';
+import connectMongoDB from './db/connectMongoDB.js';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+console.log(process.env.MONGO_URI);
 
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
-    console.log('listening on port 8000');
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+    connectMongoDB();
 })
